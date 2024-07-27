4578924adadb2e12a5fc8635ef522f26cd924083
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2024-07-27 17:03:15 +0200

    Embed views and static files

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package uipoc
 
+//package main
+
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"io"
@@ -10,6 +13,12 @@ import (
 
 	"github.com/ppacer/core/timeutils"
 )
+
+//go:embed views/*.html
+var viewsFS embed.FS
+
+//go:embed assets/* css/*
+var staticFS embed.FS
 
 type Templates struct {
 	templates *template.Template
@@ -21,7 +30,7 @@ func (t *Templates) Render(w io.Writer, name string, data any) error {
 
 func newTemplates() *Templates {
 	return &Templates{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+		templates: template.Must(template.ParseFS(viewsFS, "views/*.html")),
 	}
 }
 
@@ -56,8 +65,9 @@ func UIServer() http.Handler {
 	dagRuns := newDagRunRows()
 	templates := newTemplates()
 
-	// Serve static files
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	// Serve static files from embedded filesystem
+	mux.Handle("/assets/", http.FileServer(http.FS(staticFS)))
+	mux.Handle("/css/", http.FileServer(http.FS(staticFS)))
 
 	// Handler for the root URL
 	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
@@ -88,43 +98,43 @@ func UIServer() http.Handler {
 	return mux
 }
 
-/*
 func main() {
 	port := ":8181"
-	templates := newTemplates()
+	/*
+			templates := newTemplates()
 
-	// Serve static files
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+			// Serve static files
+			http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
-	// Handler for the root URL
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		if err := templates.Render(w, "index", dagRuns); err != nil {
-			msg := fmt.Sprintf("Failed to render template: %s", err.Error())
-			http.Error(w, msg, http.StatusInternalServerError)
+			// Handler for the root URL
+			http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+				w.Header().Set("Content-Type", "text/html; charset=utf-8")
+				if err := templates.Render(w, "index", dagRuns); err != nil {
+					msg := fmt.Sprintf("Failed to render template: %s", err.Error())
+					http.Error(w, msg, http.StatusInternalServerError)
+				}
+			})
+
+			http.HandleFunc("/dagruns", func(w http.ResponseWriter, _ *http.Request) {
+				w.Header().Set("Content-Type", "text/html; charset=utf-8")
+				if err := templates.Render(w, "dagruns", dagRuns); err != nil {
+					msg := fmt.Sprintf("Failed to render template: %s", err.Error())
+					http.Error(w, msg, http.StatusInternalServerError)
+				}
+			})
+
+			http.HandleFunc("/random-status", func(w http.ResponseWriter, _ *http.Request) {
+				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+				w.Write([]byte(randomStatus()))
+			})
+
+			http.HandleFunc("/rand", func(w http.ResponseWriter, _ *http.Request) {
+				fmt.Fprintf(w, "%d", rand.Intn(100)+8)
+			})
+
+		log.Println("Starting server on ", port)
+		if err := http.ListenAndServe(port, UIServer()); err != nil {
+			log.Fatalf("Server failed to start: %v", err)
 		}
-	})
-
-	http.HandleFunc("/dagruns", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		if err := templates.Render(w, "dagruns", dagRuns); err != nil {
-			msg := fmt.Sprintf("Failed to render template: %s", err.Error())
-			http.Error(w, msg, http.StatusInternalServerError)
-		}
-	})
-
-	http.HandleFunc("/random-status", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.Write([]byte(randomStatus()))
-	})
-
-	http.HandleFunc("/rand", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintf(w, "%d", rand.Intn(100)+8)
-	})
-
-	log.Println("Starting server on ", port)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+	*/
 }
-*/
